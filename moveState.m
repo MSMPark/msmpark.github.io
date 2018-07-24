d990f795c6e602e1bc53db511ecab66e7bb977ca
MSMPark <mpark7@gmail.com>
2018-07-23 23:11:45 -0700

    fix
    generalized select state

--- moveState.m
+++ moveState.m
@@ -4,15 +4,14 @@ function endState = moveState(startState, P)
     %Determines which state to move to based on starting state.
     chng = rand(1);
     A = P(:,startState);
-    B = sort(A);
-    diff = B(1);
-    for i=1:length(B)
+    diff = A(1);
+    for i=1:length(A)
         if chng < diff
             thresh = i;
             break
         end
-        diff = diff+B(i+1);
+        diff = diff+A(i+1);
     end
     %Returns the ending state.
-    endState = find(A == B(thresh));
+    endState = thresh;
 end
